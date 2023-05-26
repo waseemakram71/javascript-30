625e5747d2d16902f3882559073608b07a12177f
waseemakram71 <waseemsharani71@gmail.com>
2023-05-26 14:30:22 +0500

    project-27 copy to clipboard

---
+++ project-27-Copy-To-Clipboard/index.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button');
+button.addEventListener('click',function() {
+  let input = document.querySelector('input');
+  input.select();
+  document.execCommand('copy');
+
+})
+wwwwwwwasseeeeeeee
