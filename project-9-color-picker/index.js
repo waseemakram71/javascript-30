2ea0baf663062fa4fe4684208e7fb1fed32d273e
waseemakram71 <waseemsharani71@gmail.com>
2023-05-26 13:56:45 +0500

    project-9-color-picker

---
+++ project-9-color-picker/index.js
@@ -0,0 +1,5 @@
+const colorPicker = document.getElementById('color-picker');
+colorPicker.addEventListener('input' , ()=>{
+  const colorSelector = colorPicker.value;
+  document.body.style.background = colorSelector;
+})
