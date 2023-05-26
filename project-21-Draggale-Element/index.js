ec9f7ce765f6afb886c85f10d6d5f33fd28d1947
waseemakram71 <waseemsharani71@gmail.com>
2023-05-26 14:23:58 +0500

    project 21- draggle element

---
+++ project-21-Draggale-Element/index.js
@@ -0,0 +1,18 @@
+const wrapper = document.querySelector('.wrapper');
+function onDrag({movementX,movementY}) {
+  let getStyle = window.getComputedStyle(wrapper);
+  let left = parseInt(getStyle.left);
+  let top = parseInt(getStyle.top);
+  wrapper.style.left = `${left + movementX}px`
+  wrapper.style.top = `${top + movementY}px`
+}
+
+wrapper.addEventListener('mousedown',()=>{
+  wrapper.classList.add('active');
+  wrapper.addEventListener('mousemove',onDrag);
+});
+
+wrapper.addEventListener('mouseup',()=>{
+  wrapper.classList.remove('active');
+  wrapper.removeEventListener('mousemove',onDrag);
+});
